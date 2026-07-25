90c6cd3d6046f67c25dc92463873dc1c909e0908
Gabriel de Souza <gabrielferreira20083007@gmail.com>
2026-07-24 21:42:23 -0300

    quarto commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_app/utilitarios/tipografia.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Login(),
+        body: SafeArea(child: Login()),
       ),
     );
   }
@@ -19,22 +20,41 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        FlutterLogo(size: 18,),
-        Text("ChatSENAC"),
-        Text("Entre na sua conta"),
-        Text("Coloque o seu email e senha para logar"),
+        Row(children: [FlutterLogo(size: 18,),
+          Text("ChatSENAC"),
+          SizedBox(height: 32,)
+        ],
+        ),
+        //Titulos
+
+        Text("Entre na sua conta", style: Tipografia.h1
+        ),
+        Text("Coloque o seu email e senha para logar" , style: Tipografia.subtitulo),
+
+
+        // Campos
         Text("Email"),
         TextField(),
         Text("Senha"),
         TextField(),
-        InkWell(child: Text("Esqueceu a senha?")),
+        InkWell(child: Text("Esqueceu a senha?", textAlign: TextAlign.right,
+          style: Tipografia.Link,)),
+
+        //Botões
         ElevatedButton(onPressed: null, child: Text("entrar")),
-        Text("Ou"),
+        Text("Ou", textAlign: TextAlign.center,),
         ElevatedButton(onPressed: null, child: Text("Continuar com o Google")),
-        ElevatedButton(onPressed: null, child: Text("Continuar com o Facebook")),
-        Text("Não tem uma conta?"),
-        InkWell(child: Text("Cadastre-se")),
+        ElevatedButton(
+            onPressed: null, child: Text("Continuar com o Facebook")),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Não tem uma conta?"),
+            InkWell(child: Text("Cadastre-se",
+            style: Tipografia.Link)),
+          ],)
       ],
     );
   }
